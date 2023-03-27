117b08792cb53ef8ed9801892044621e8c5726f4
mierku <2764002430@qq.com>
2023-03-27 18:40:00 +0800

    merge: 同步main

--- pages/index.vue
+++ pages/index.vue
@@ -16,21 +16,3 @@ async function test() {
   <button @click="test()">发送请i去</button>
   <NuxtLayout></NuxtLayout>
 </template>
-<script setup lang="ts">
-// definePageMeta({
-// 	documentDriven: false,
-
-import { recordIP, recordPV, recordUV } from '~~/composables/api/useWebLog'
-
-// })
-async function test() {
-  const { data } = await recordPV()
-  await recordIP()
-  await recordUV()
-  console.log(data.value)
-}
-</script>
-<template>
-  <button @click="test()">发送请i去</button>
-  <NuxtLayout></NuxtLayout>
-</template>
